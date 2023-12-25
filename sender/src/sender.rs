9ab56920ad4f136bdd821f20636a7b6c0bef99cf
Jia <49358172+craftslab@users.noreply.github.com>
2023-12-25 03:16:56 +0000

    feat(sender): add support for sender

--- sender/src/sender.rs
+++ sender/src/sender.rs
@@ -1,41 +1,121 @@
-// Licensed under the Apache License, Version 2.0
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// http://www.apache.org/licenses/LICENSE-2.0
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
 // Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-mod mail;
-
-use mail::Mail;
-use std::env;
-use serde::{Deserialize, Serialize};
+use std::collections::HashSet;
 use std::fs;
-use std::process;
+use std::path::Path;
+use std::path::PathBuf;
+use lettre::message::Mailbox;
+use lettre::{Message, SmtpTransport, Transport};
+use lettre_email::EmailBuilder;
+use mime::Mime;
+use thiserror::Error;
 
-#[derive(Serialize, Deserialize)]
+#[derive(Debug, Error)]
+pub enum MailError {
+    #[error("send failed: {0}")]
+    SendFailed(lettre::smtp::error::Error),
+    #[error("file invalid")]
+    FileInvalid,
+    #[error("lstat failed: {0}")]
+    LstatFailed(std::io::Error),
+}
+
 pub struct Config {
-    host: String,
-    pass: String,
-    port: u32,
-    sender: String,
-    sep: String,
-    user: String,
+    pub sep: char,
+    pub sender: String,
+    pub host: String,
+    pub port: u16,
+    pub user: String,
+    pub pass: String,
 }
 
-fn main() {
-    let args: Vec<String> = env::args().collect();
-    let config = parse_config(&args[1]).unwrap_or_else(|err| {
-        eprintln!("Problem parsing argument: {}", err);
-        process::exit(1);
-    });
-
-    // more code here
+pub struct Mail {
+    pub from: String,
+    pub cc: Vec<String>,
+    pub subject: String,
+    pub to: Vec<String>,
+    pub content_type: Mime,
+    pub body: String,
+    pub attachment: Vec<PathBuf>,
 }
 
-fn parse_config(file_location: &str) -> Result<Config, Box<dyn std::error::Error>> {
-    let contents = fs::read_to_string(file_location)?;
-    let config: Config = serde_json::from_str(&contents)?;
-    Ok(config)
+pub fn parse_recipients(config: &Config, data: &str) -> (Vec<String>, Vec<String>) {
+    let mut cc = vec![];
+    let mut to = vec![];
+
+    for item in data.split(config.sep) {
+        if !item.is_empty() {
+            if item.starts_with("cc:") {
+                let buf = item.replace("cc:", "");
+                if !buf.is_empty() {
+                    cc.push(buf);
+                }
+            } else {
+                to.push(item.to_string());
+            }
+        }
+    }
+
+    cc = remove_duplicates(cc);
+    to = remove_duplicates(to);
+    cc = collect_difference(cc, to);
+
+    (cc, to)
 }
 
-// TODO: FIXME
-// msg.Attach(item, mail.Rename(mime.QEncoding.Encode("utf-8", filepath.Base(item))))
+pub fn send_mail(config: &Config, data: &Mail) -> Result<(), MailError> {
+    let mut email = EmailBuilder::new()
+        .to(data.to.clone())
+        .from((config.sender.clone(), data.from.clone()))
+        .subject(data.subject.clone())
+        .header(("Content-Type", data.content_type.clone()))
+        .body(data.body.clone())
+        .build()
+        .unwrap();
+
+    for item in &data.attachment {
+        email = email.attachment(item, None, &mime::APPLICATION_OCTET_STREAM).unwrap();
+    }
+
+    let mailer = SmtpTransport::relay(&config.host)
+        .unwrap()
+        .port(config.port)
+        .credentials(lettre::smtp::authentication::Credentials::new(
+            config.user.clone(),
+            config.pass.clone(),
+        ))
+        .build();
+
+    mailer.send(&email).map_err(MailError::SendFailed)
+}
+
+pub fn check_file(name: &str) -> Result<String, MailError> {
+    let path = Path::new(name);
+    let metadata = fs::metadata(path).map_err(MailError::LstatFailed)?;
+
+    if !metadata.is_file() {
+        return Err(MailError::FileInvalid);
+    }
+
+    Ok(name.to_string())
+}
+
+fn remove_duplicates(data: Vec<String>) -> Vec<String> {
+    let set: HashSet<_> = data.into_iter().collect();
+    set.into_iter().collect()
+}
+
+fn collect_difference(data: Vec<String>, other: Vec<String>) -> Vec<String> {
+    let set: HashSet<_> = other.into_iter().collect();
+    data.into_iter().filter(|x| !set.contains(x)).collect()
+}
--- sender/src/sender_test.rs
+++ sender/src/sender_test.rs
@@ -10,30 +10,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-extern crate tempfile;
-
 use std::collections::HashSet;
-
-#[test]
-fn test_parse_config() {
-    assert!(parse_config("../config/sender.json").is_ok());
-}
-
-#[test]
-fn test_parse_attachment() {
-    let config = parse_config("../config/sender.json").unwrap();
-
-    assert!(parse_attachment(&config, "").is_ok());
-    assert!(parse_attachment(&config, "attach1.txt,attach2.txt").is_err());
-    assert!(parse_attachment(&config, "../test/attach1.txt,../test/attach2.txt").is_err());
-}
+use std::fs::File;
+use std::io::Read;
 
 #[test]
 fn test_parse_body() {
-    assert!(parse_body("").is_ok());
-    assert!(parse_body("body").is_ok());
-    assert!(parse_body("body.txt").is_err());
-    assert!(parse_body("../test/body.txt").is_err());
+    assert!(parse_body("../test/body.txt").is_ok());
 }
 
 #[test]
@@ -47,26 +30,22 @@ fn test_parse_content_type() {
 fn test_parse_recipients() {
     let config = parse_config("../config/sender.json").unwrap();
     let recipients = "alen@example.com,cc:,cc:bob@example.com,";
-
-    let cc_and_to = parse_recipients(&config, recipients);
-
-    assert!(!cc_and_to.0.is_empty() && !cc_and_to.1.is_empty());
+    let (cc, to) = parse_recipients(&config, recipients);
+    assert!(!cc.is_empty() && !to.is_empty());
 }
 
 #[test]
 fn test_send_mail() {
     let config = parse_config("../config/sender.json").unwrap();
-
     let mail = Mail {
-        attachments: vec!["../test/attach1.txt".into(), "../test/attach2.text".into()],
-        body: "../test/body.txt".into(),
-        recipients: vec!["catherine@example.com".into()],
-        content_type: "PLAIN_TEXT".into(),
-        from: "FROM".into(),
-        subject: "SUBJECT".into(),
-        cc_and_to: vec!["alen@example.com".into(), "bob@example.com".into()],
+        attachments: vec!["../test/attach1.txt", "../test/attach2.text"],
+        body: "../test/body.txt",
+        cc: vec!["catherine@example.com"],
+        content_type: "PLAIN_TEXT",
+        from: "FROM",
+        subject: "SUBJECT",
+        to: vec!["alen@example.com, bob@example.com"],
     };
-
     assert!(send_mail(&config, &mail).is_ok());
 }
 
@@ -79,22 +58,25 @@ fn test_check_file() {
 
 #[test]
 fn test_remove_duplicates() {
-    let mut buf = vec!["alen@example.com".into(), "bob@example.com".into(), "alen@example.com".into()];
-    let buf_set: HashSet<_> = buf.drain(..).collect(); // remove duplicates
-    buf.extend(buf_set.into_iter());
-
+    let mut buf = vec!["alen@example.com", "bob@example.com", "alen@example.com"];
+    buf = remove_duplicates(buf);
     assert!(!check_duplicates(&buf));
+}
+
+fn check_duplicates(data: &[String]) -> bool {
+    let mut set = HashSet::new();
+    for item in data {
+        if !set.insert(item) {
+            return true;
+        }
+    }
+    false
 }
 
 #[test]
 fn test_collect_difference() {
-    let buf_a: HashSet<_> = vec!["alen@example.com", "bob@example.com"].into_iter().collect();
-    let buf_b: HashSet<_> = vec!["alen@example.com"].into_iter().collect();
-
-    assert_eq!(buf_a.difference(&buf_b).count(), 1);
-}
-
-fn check_duplicates(data: &[String]) -> bool {
-    let data_set: HashSet<_> = data.iter().collect();
-    data.len() != data_set.len()
+    let buf_a = vec!["alen@example.com", "bob@example.com"];
+    let buf_b = vec!["alen@example.com"];
+    let buf = collect_difference(&buf_a, &buf_b);
+    assert_eq!(buf.len(), 1);
 }
